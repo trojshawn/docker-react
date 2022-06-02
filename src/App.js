8b91c8a94fed253d6cb8aa6fecc8effce23de08e
trojshawn <shawn.mccaughan@troj.ai>
2022-06-02 10:33:42 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Helluva whootie
+          Helluva whootie (feature branch change)
         </p>
         <a
           className="App-link"
